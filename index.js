db05c1154ffc8afa1e1915809da074817cf0be85
Mike Blackson <mkblcksn@gmail.com>
2022-03-31 19:15:30 -0500

    working on cards

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
+const employee = require('./lib/employee');
+const engineer = require('./lib/engineer');
+const manager = require('./lib/manager');
+const intern = require('./lib/intern');
 
-const roleArray = [ 'Manager', 'Engineer', 'Intern']
+choices: [ 'Manager', 'Engineer', 'Intern']
 
 function buildTeam ( ) {
     inquirer.prompt ([
         {
             type: 'input',
-            message: 'What is the name of your employee?',
+            message: 'What is the name of your manager?',
             name: 'employee'
         },
         {
@@ -21,12 +25,46 @@ function buildTeam ( ) {
             name: 'email'
         },
         {
-            type: 'list', 
-            message: 'What is their role?',
-            name: 'role',
-            choices: roleArray
+            type: 'input', 
+            message: 'What is their office number?',
+            name: 'officeNumber'
+        },
+        {
+            type: 'input',
+            message: 'what is your employee name?',
+            name: 'employee'
+        },
+        {
+            type: 'input',
+            message: 'what is your employee id?',
+            name: 'id'
+        },
+        {
+            type: 'input',
+            message: 'what is your employee email?',
+            name: 'email'
+        },
+        {
+            type: 'list',
+            message: 'Would you like to add another team member?',
+            name: 'teamMember',
+            choices: [ 'yes', 'no']
+        },
+    ]).then(response => {
+        if(response === true) {
+            getEmployeeData( ) 
+        }else{
+            fs.writeFile()
         }
-    ])
+
+        {
+            type: 'list',
+            message: 'what is your employee role?',
+            name: 'role',
+            choices: [ 'Engineer', 'Intern']
+        } 
+    ])         
 }
+    
 
 buildTeam( )
